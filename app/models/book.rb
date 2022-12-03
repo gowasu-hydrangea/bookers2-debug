aec83b753579cf255cef8ecfdc564c24d6c814cf
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-12-03 14:30:17 +0000

    [Finish]1203

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
-  has_many :user
+  # has_many :user
+  # ア・２＞9章
+  # アソシエーション
+  belongs_to :user
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  # belongs_to :books
   has_one_attached :profile_image
+  
+  has_many :books, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction,
+  length: { maximum: 50 }
 
   
   
