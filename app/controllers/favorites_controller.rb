b8f41d9f45627abf48b7cf3e18c786735c362973
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-12-05 12:45:27 +0000

    [Finish]favorite&comment1205

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,4 @@
-class FavoriteController < ApplicationController
-  
-  # ア・２＞19章
-  # createアクションとdestroyアクションを追加
+class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
